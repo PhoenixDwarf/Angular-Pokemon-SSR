48493e0cf14bfb1771ca18cfcfc9fa6dca7fc787
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-02-04 14:16:31 -0500

    feat: update server route for Pokemon detail to use prerendering with fallback

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,9 +1,17 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { RenderMode, ServerRoute, PrerenderFallback } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'pokemon-detail/:id',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Prerender,
+    // Fallback to avoid 404 errors when users land on ids higher than 20
+    // Server rendering takes over will correctly render the page
+    fallback: PrerenderFallback.Server,
+    // Here we add a function to prerender 20 pages on build time since we are using SSG
+    getPrerenderParams: async () => {
+      // Generate ids from 1 to 20
+      return Array.from({ length: 20 }, (_, i) => ({ id: (i + 1).toString() }));
+    },
   },
   {
     path: '**',
