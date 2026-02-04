a229fca66dcc541833b7cbbf53e403e06b14508a
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-02-03 19:20:54 -0500

    feat: add platform detection for browser-specific operations in PricingPage

--- src/app/pages/pricing/pricing-page.ts
+++ src/app/pages/pricing/pricing-page.ts
@@ -1,4 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
@@ -10,8 +11,18 @@ import { Title, Meta } from '@angular/platform-browser';
 export default class PricingPage implements OnInit {
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+    // With the following validation we can use browser objects without generating server errors
+    // BUT FOR THINGS SUCH AS SETTING TITLES AND META TAGS DO IT BY INJECTING THE RESPECTIVE SERVICE FROM platform-browser
+
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+
+    // console.log(document);
+
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({ name: 'description', content: 'This is the pricing page' });
     this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
