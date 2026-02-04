896aed1c07a29cbeb5075d44768376913eb8767f
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-02-04 13:19:59 -0500

    feat: enhance Pokemon detail page to check platform before alerting on invalid ID

--- src/app/pages/pokemon-detail/pokemon-detail-page.ts
+++ src/app/pages/pokemon-detail/pokemon-detail-page.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { PokemonDetailAPIResponse } from '../../pokemons/interfaces';
 import { Pokemons } from '../../pokemons/services/pokemons';
 import { ActivatedRoute, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'pokemon-detail-page',
@@ -13,14 +14,17 @@ export default class PokemonDetailPage implements OnInit {
   private pokemonService = inject(Pokemons);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
+  private platform = inject(PLATFORM_ID);
 
   public pokemon = signal<PokemonDetailAPIResponse | null>(null);
 
   ngOnInit(): void {
     const id = +(this.route.snapshot.paramMap.get('id') ?? '');
     if (isNaN(id) || id < 1) {
-      alert('Wrong pokemon ID');
-      this.router.navigate(['/']);
+      if (isPlatformBrowser(this.platform)) {
+        alert('Wrong pokemon ID');
+        this.router.navigate(['/']);
+      }
       return;
     }
 
