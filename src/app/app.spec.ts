700c2ab28d77535a143cf825a70f0b07b2f3a3c8
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-02-05 12:12:22 -0500

    test: add snapshot for App component and improve test setup with mock Navbar

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,24 +1,92 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { provideRouter } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Navbar } from './shared/components/navbar/navbar';
+
+@Component({
+  selector: 'app-navbar',
+  template: `<nav class="test-class"><a href="test-link">Test Link</a></nav>`,
+})
+class MockNavbar {}
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let app: App;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-      // Following line fixes the error "No provider found for `ActivatedRoute`."
-      // Adding it will make this an integration test rather than a unit test.
-      // This error happens because the App component imports the Navbar component that has inner imports from router module
-      providers: [provideRouter([])],
-    }).compileComponents();
+    // Options to handle inner components with dependencies and fix the error: "No provider found for `xxx`."
+    // This error happens because the App component imports the Navbar component that has inner imports from router module
+    //
+    // --------
+    //
+    // #1 - Provide components, modules, services, etc
+    // Following example will make it an integration test rather than a unit test.
+    // await TestBed.configureTestingModule({
+    //   imports: [App],
+    //   providers: [provideRouter([])],
+    // }).compileComponents();
+    //
+    // --------
+    //
+    // #2 - Override (add and remove) inner component with a mock
+    //
+    // await TestBed.configureTestingModule({
+    //   imports: [App],
+    // })
+    //   .overrideComponent(App, {
+    //     add: {
+    //       imports: [MockNavbar],
+    //     },
+    //     remove: {
+    //       imports: [Navbar],
+    //     },
+    //   })
+    //   .compileComponents();
+    //
+    // --------
+    //
+    // #3 - Override (set) inner component with a mock
+    // Warning !!!
+    // Using this method will replace all imports with the ones given in the imports array.
+    // This will not throw errors and stop the test if there are missing components, it will just log them
+    // into the console instead (this can also be avoided by adding CUSTOM_ELEMENTS_SCHEMA, not recommended).
+
+    TestBed.overrideComponent(App, {
+      set: {
+        imports: [MockNavbar],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      },
+    });
+
+    // --------
+
+    fixture = TestBed.createComponent(App);
+    app = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
+  it('should create the app', () => expect(app).toBeTruthy());
 
-    console.log(fixture.nativeElement.innerHTML);
+  it('Should render the navbar and router-outlet', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
 
-    expect(app).toBeTruthy();
+    expect(compiled.querySelector('app-navbar')).toBeTruthy();
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
+
+  // Following test is not longer recommended
+
+  // Snapshot -> Photograph of HTML
+
+  // This test creates an snapshot if there wasn't one before and saves it into src/app/_snapshots_
+  // If it finds a snapshot it will compare it to the current html output.
+
+  // Since nowadays we use tools such as tailwind or boostrap helper classes the HTML may vary over time
+  // and the test will probably fail frequently
+  // Note: If test fails and the modified HTML is the desired one, vitest lets us update the snapshot by pressing "U"
+
+  it('should match snapshot', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    console.log(compiled.innerHTML);
+
+    expect(compiled.innerHTML).toMatchSnapshot();
   });
 });
